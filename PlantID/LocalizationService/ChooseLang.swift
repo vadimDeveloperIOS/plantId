c65a51833a2c21633f27e1c8e333bbc720778659
Vadim <91436203+VadimWictorovich@users.noreply.github.com>
2025-08-30 01:24:06 +0300

    AIAssistantCell готовый

--- PlantID/LocalizationService/ChooseLang.swift
+++ PlantID/LocalizationService/ChooseLang.swift
@@ -12,5 +12,6 @@ enum TextForHomeScene {
     static let search = "search".localized
     static let readMore = "read_more".localized
     static let carePlan = "care_plan".localized
-    
+    static let askYourPlantAIAssistant = "ask_your_plant_ai_assistant".localized
+    static let aiAssistantIsReadyToHelp = "ai_assistant_is_ready_to_help".localized
 }
